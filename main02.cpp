526f4a3ec582f2623e67ec0f6b2764fb0b1eefe5
KRONASDarK <58785979+KRONASDarK@users.noreply.github.com>
2025-09-08 22:10:37 +0300

    Create main02.cpp

---
+++ main02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	string text = ""s;
+	cout << "Введите слово:"s << endl;
+	cin >> text;
+	cout << "Вы ввели:"s << endl;
+	cout << text << endl;
+}
