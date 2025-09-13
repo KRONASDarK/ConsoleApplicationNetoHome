91c95189b2975abf46ca7e21f7b44b4db453cbe3
KRONASDarK <58785979+KRONASDarK@users.noreply.github.com>
2025-09-13 13:51:08 +0300

    Create main3.03.cpp

---
+++ main3.03.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i];
+        if(i != size - 1) std::cout << ", ";
+    }
+}
+
+void bubbleSortForward(int arr[], int size) {
+    bool swapped;
+    for (int i = 0; i < size - 1; ++i) {
+        swapped = false;
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+// Обратная сортировка пузырьком
+void bubbleSortReverse(int arr[], int size) {
+    bool swapped;
+    for (int i = 0; i < size - 1; ++i) {
+        swapped = false;
+        for (int j = 0; j < size - i - 1; ++j) {
+            if (arr[j] < arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped) break;
+    }
+}
+
+int main() {
+    srand(time(NULL)); // Используем текущее время для генерации случайных чисел
+    
+    const int SIZE = 10;
+    int array[SIZE];
+    
+    for (int i = 0; i < SIZE; ++i) {
+        array[i] = rand() % 200 - 100; // Случайные значения от -100 до 100
+    }
+    
+    std::cout << "Массив до сортировки: ";
+    printArray(array, SIZE);
+    std::cout << std::endl;
+    
+    // Выполняем прямую сортировку
+    bubbleSortForward(array, SIZE);
+   std:: cout << "Массив после прямой сортировки: ";
+    printArray(array, SIZE);
+    std::cout << std::endl;
+    
+    // Выполняем обратную сортировку
+    bubbleSortReverse(array, SIZE);
+    std::cout << "Массив после обратной сортировки: ";
+    printArray(array, SIZE);
+    std::cout << std::endl;
+    
+    return 0;
+}
