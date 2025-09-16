5f1caf213afc30ec39e2502acfaa3ae67a8304fa
KRONASDarK <58785979+KRONASDarK@users.noreply.github.com>
2025-09-16 21:59:46 +0300

    Create main5.01.cpp

---
+++ main5.01.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale.h>
+
+void swap(int &a, int &b) {
+	int x = a;
+	a = b;
+	b = x;
+}
+
+int main()
+{
+	int a = 5, b = 8;
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	swap(a, b);
+
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+
+	return 0;
+}
