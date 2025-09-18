e7d8bbd614f19f8e7b490dc79f7c8fc188ba144c
KRONASDarK <58785979+KRONASDarK@users.noreply.github.com>
2025-09-18 16:34:26 +0300

    Update main6.01.cpp

--- main6.01.cpp
+++ main6.01.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
 
-int calc(int x, int y, char op) {
-    switch(op) {
-        case '+': return x + y; break;
-        case '-': return x - y; break;
-        case '*': return x * y; break;
-        case '/': 
-            if(y != 0) return x / y;
-            else throw std::runtime_error("Ошибка: Деление на ноль"); 
-            break;
-        default: throw std::invalid_argument("Неверный оператор");
+namespace one {
+    int calc(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+}
+
+namespace two {
+    int calc(int a, int b) {
+        int diff = a - b;
+        return diff;
+    }
+}
+
+namespace three {
+    int calc(int a, int b) {
+        int mult = a * b;
+        return mult;
+    }
+}
+
+namespace four {
+    int calc(int a, int b) {
+        int div = a / b;
+        return div;
     }
 }
 
 int main() {
-    int x = 6;
-    int y = 9;
-    try {
-        std::cout << calc(x, y, '+') << std::endl;
-        std::cout << calc(x, y, '-') << std::endl;
-        std::cout << calc(x, y, '*') << std::endl;
-        std::cout << calc(x, y, '/') << std::endl;
-        std::cout << calc(x, y, '%') << std::endl;
-    } catch(const std::exception& ex) {
-        std::cerr << "Исключение: " << ex.what() << std::endl;
-    }
-    
+    int a = 6, b = 9;
+    std::cout << "a = " << a << ", " << "b = " << b << std::endl;
+
+    std::cout << "Сложение: " << one::calc(a, b) << std::endl;
+    std::cout << "Вычитание: " << two::calc(a, b) << std::endl;
+    std::cout << "Умножение: " << three::calc(a, b) << std::endl;
+    std::cout << "Деление: " << four::calc(a, b) << std::endl;
+
     return 0;
 }
